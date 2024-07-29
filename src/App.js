4bf78c7b196ec39a961c307ae019060d7f95e170
Ganesh Kolekar <kolekarganesh5151@gmail.com>
2024-07-29 19:54:53 +0530

    created routes and page components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 
 import './App.css';
-
+import { Route,Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Admin from './pages/Admin';
+import Student from './pages/Student';
+import Login from './pages/Login';
 function App() {
   return (
     <div className="App">
-      welcome
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/admin' element={<Admin/>}/>
+        <Route path='/student' element={<Student/>}/>
+      </Routes>
     </div>
   );
 }
